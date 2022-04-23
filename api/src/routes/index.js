890abe7f3cf46e8fbb7e4d6b24866533a0a2fab0
Fedexaz <fedexaz3@gmail.com>
2022-04-23 13:52:12 -0300

    Repositorio cambiado a privado

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,8 @@
 const app = require('express').Router();
 const authRoutes = require('./auth');
-const dashboadRoutes = require('./dashboard');
 const verifyToken = require('./validate-token');
 
-app.use('/dashboard', verifyToken, dashboadRoutes)
+//app.use('/dashboard', verifyToken, dashboadRoutes)
 app.use('/user', authRoutes);
 
 module.exports = app;

75ec89de768d705a87eb8a53222f5ceb1b4952e9
Fedexaz <fedexaz3@gmail.com>
2022-04-25 00:10:05 -0300

    Agregadas bebidas en la base de datos

--- api/src/UTILS/saveToDB.js
+++ api/src/UTILS/saveToDB.js
@@ -1,72 +1,91 @@
-const Bebida = require('./bebidas.model');
+const Bebidas = require('../models/Bebidas.model');
 const fs = require('fs');
 
 const saveToDB = async () => {
     try {
-        const loadFile = fs.readFileSync(__dirname + '/bebidas.json', 'utf-8');
+        const loadFile = fs.readFileSync(__dirname + '/bebidas2.json', 'utf-8');
         const bebida = JSON.parse(loadFile);
 
         for (let i = 0; i < bebida.length; i++) {
-            let datos = new Bebida({
-                idDrink: bebida[i].idDrink,
-                strDrink: bebida[i].strDrink,
-                strDrinkAlternate: bebida[i].strDrinkAlternate,
-                strTags: bebida[i].strTags,
-                strVideo: bebida[i].strVideo,
-                strCategory: bebida[i].strCategory,
-                strIBA: bebida[i].strIBA,
-                strAlcoholic: bebida[i].strAlcoholic,
-                strGlass: bebida[i].strGlass,
-                strInstructions: bebida[i].strInstructions,
-                "strInstructionsZH-HANS": bebida[i]["strInstructionsZH-HANS"],
-                "strInstructionsZH-HANT": bebida[i]["strInstructionsZH-HANT"],
-                strDrinkThumb: bebida[i].strDrinkThumb,
-                strIngredient1: bebida[i].strIngredient1,
-                strIngredient2: bebida[i].strIngredient2,
-                strIngredient3: bebida[i].strIngredient3,
-                strIngredient4: bebida[i].strIngredient4,
-                strIngredient5: bebida[i].strIngredient5,
-                strIngredient6: bebida[i].strIngredient6,
-                strIngredient7: bebida[i].strIngredient7,
-                strIngredient8: bebida[i].strIngredient8,
-                strIngredient9: bebida[i].strIngredient9,
-                strIngredient10: bebida[i].strIngredient10,
-                strIngredient11: bebida[i].strIngredient11,
-                strIngredient12: bebida[i].strIngredient12,
-                strIngredient13: bebida[i].strIngredient13,
-                strIngredient14: bebida[i].strIngredient14,
-                strIngredient15: bebida[i].strIngredient15,
-                strMeasure1: bebida[i].strMeasure1,
-                strMeasure2: bebida[i].strMeasure2,
-                strMeasure3: bebida[i].strMeasure3,
-                strMeasure4: bebida[i].strMeasure4,
-                strMeasure5: bebida[i].strMeasure5,
-                strMeasure6: bebida[i].strMeasure6,
-                strMeasure7: bebida[i].strMeasure7,
-                strMeasure8: bebida[i].strMeasure8,
-                strMeasure9: bebida[i].strMeasure9,
-                strMeasure10: bebida[i].strMeasure10,
-                strMeasure11: bebida[i].strMeasure11,
-                strMeasure12: bebida[i].strMeasure12,
-                strMeasure13: bebida[i].strMeasure13,
-                strMeasure14: bebida[i].strMeasure14,
-                strMeasure15: bebida[i].strMeasure15,
-                strImageSource: bebida[i].strImageSource,
-                strImageAttribution: bebida[i].strImageAttribution,
-                strCreativeCommonsConfirmed: bebida[i].strCreativeCommonsConfirmed,
-                likes: 0,
-                comments: 0,
-                createdInPage: true,
-                idCreator: userID  
+            let datos = new Bebidas({
+                nombre: bebida[i].nombre,
+                categoria: bebida[i].categoria,
+                tieneAlcohol: bebida[i].tieneAlcohol,
+                dondeSeSirve: bebida[i].dondeSeSirve,
+                instrucciones: bebida[i].instrucciones,
+                ingredientes: bebida[i].ingredientes,
+                cantidades: bebida[i].cantidades,
+                img: bebida[i].img,
+                imgConDerechos: bebida[i].imgConDerechos
             });
             await datos.save();
-            console.log(`${datos.strDrink} (ID: ${datos.idDrink}) guardada en la base de datos!`);
-        }
+            console.log(`${datos.nombre} (ID: ${datos._id}) guardada en la base de datos!`);
+        } 
 
         console.log(" [ ! ] Datos guardados correctamente en la Base de Datos!");
     } catch (error) {
         console.log(error);
     }
 };
+
+/* 
+const transform = () => {
+    const loadFile = fs.readFileSync(__dirname + '/bebidas.json', 'utf-8');
+    const bebidas = JSON.parse(loadFile);
+    const bebidasProcesadas = [];
+
+    for (let i = 0; i < bebidas.length; i++) {
+
+        let ingredientes = '';
+        bebidas[i].strIngredient1 ? ingredientes += bebidas[i].strIngredient1 : null;
+        bebidas[i].strIngredient2 ? ingredientes += ',' +  bebidas[i].strIngredient2 : null;
+        bebidas[i].strIngredient3 ? ingredientes += ',' +  bebidas[i].strIngredient3 : null;
+        bebidas[i].strIngredient4 ? ingredientes += ',' +  bebidas[i].strIngredient4 : null;
+        bebidas[i].strIngredient5 ? ingredientes += ',' +  bebidas[i].strIngredient5 : null;
+        bebidas[i].strIngredient6 ? ingredientes += ',' +  bebidas[i].strIngredient6 : null;
+        bebidas[i].strIngredient7 ? ingredientes += ',' +  bebidas[i].strIngredient7 : null;
+        bebidas[i].strIngredient8 ? ingredientes += ',' +  bebidas[i].strIngredient8 : null;
+        bebidas[i].strIngredient9 ? ingredientes += ',' +  bebidas[i].strIngredient9 : null;
+        bebidas[i].strIngredient10 ? ingredientes += ',' +  bebidas[i].strIngredient10 : null;
+        bebidas[i].strIngredient11 ? ingredientes += ',' +  bebidas[i].strIngredient11 : null;
+        bebidas[i].strIngredient12 ? ingredientes += ',' +  bebidas[i].strIngredient12 : null;
+        bebidas[i].strIngredient13 ? ingredientes += ',' +  bebidas[i].strIngredient13 : null;
+        bebidas[i].strIngredient14 ? ingredientes += ',' +  bebidas[i].strIngredient14 : null;
+        bebidas[i].strIngredient15 ? ingredientes += ',' +  bebidas[i].strIngredient15 : null;
+
+        let cantidades = '';
+        bebidas[i].strMeasure1 ? cantidades += bebidas[i].strMeasure1 : null;
+        bebidas[i].strMeasure2 ? cantidades += ',' +  bebidas[i].strMeasure2 : null;
+        bebidas[i].strMeasure3 ? cantidades += ',' +  bebidas[i].strMeasure3 : null;
+        bebidas[i].strMeasure4 ? cantidades += ',' +  bebidas[i].strMeasure4 : null;
+        bebidas[i].strMeasure5 ? cantidades += ',' +  bebidas[i].strMeasure5 : null;
+        bebidas[i].strMeasure6 ? cantidades += ',' +  bebidas[i].strMeasure6 : null;
+        bebidas[i].strMeasure7 ? cantidades += ',' +  bebidas[i].strMeasure7 : null;
+        bebidas[i].strMeasure8 ? cantidades += ',' +  bebidas[i].strMeasure8 : null;
+        bebidas[i].strMeasure9 ? cantidades += ',' +  bebidas[i].strMeasure9 : null;
+        bebidas[i].strMeasure10 ? cantidades += ',' +  bebidas[i].strMeasure10 : null;
+        bebidas[i].strMeasure11 ? cantidades += ',' +  bebidas[i].strMeasure11 : null;
+        bebidas[i].strMeasure12 ? cantidades += ',' +  bebidas[i].strMeasure12 : null;
+        bebidas[i].strMeasure13 ? cantidades += ',' +  bebidas[i].strMeasure13 : null;
+        bebidas[i].strMeasure14 ? cantidades += ',' +  bebidas[i].strMeasure14 : null;
+        bebidas[i].strMeasure15 ? cantidades += ',' +  bebidas[i].strMeasure15 : null;
+        
+        bebidasProcesadas.push({
+            nombre: bebidas[i].strDrink,
+            categoria: bebidas[i].strCategory,
+            tieneAlcohol: bebidas[i].strAlcoholic,
+            dondeSeSirve: bebidas[i].strGlass,
+            instrucciones: bebidas[i].strInstructions,
+            ingredientes,
+            cantidades,
+            img: bebidas[i].strDrinkThumb,
+            imgConDerechos: bebidas[i].strCreativeCommonsConfirmed
+        });
+    }
+    fs.writeFileSync(__dirname + '/bebidas2.json', JSON.stringify(bebidasProcesadas), 'utf-8');
+    console.log("datos de bebidas procesados correctamente!");
+};
+
+//transform(); */
 
 module.exports = saveToDB;
--- api/src/services/myDrink.service.js
+++ api/src/services/myDrink.service.js
@@ -1,19 +1,39 @@
 const Bebidas = require('../models/Bebidas.model');
 
 const getMyDrinkService = async (creatorID) => {
-
+    try {
+        const drinks = await Bebidas.find({ creatorID });
+        return drinks;
+    } catch (error) {
+        throw error;
+    }
 };
 
 const addMyDrinkService = async (data) => {
-
+    try {
+        const bebida = new Bebidas(data);
+        await bebida.save();
+    } catch (error) {
+        throw error;
+    }  
 };
 
 const editMyDrinkService = async (creatorID, drinkID) => {
 
 };
 
-const deleteMyDrinkService = async (creatorID, drinkID) => {
-    
+const deleteMyDrinkService = async (userID, drinkID) => {
+    try {
+        const drink = await Bebidas.findOne(drinkID);
+        if(drink.creatorID === userID) {
+            await Bebidas.findByIdAndDelete(drinkID);
+        }   
+        else {
+            throw Error("La bebida no es tuya");
+        }
+    } catch (error) {
+        throw error;
+    }
 };
 
 module.exports = {

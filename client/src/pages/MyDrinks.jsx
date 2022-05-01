3904cf1182c6b52b406bb0b3fdb85b328e0089ff
Fede Lahoz <fedexaz3@gmail.com>
2022-04-30 21:13:07 -0300

    Update MyDrinks.jsx

--- client/src/pages/MyDrinks.jsx
+++ client/src/pages/MyDrinks.jsx
@@ -124,10 +124,13 @@ export default function MyDrinks() {
                 misBebidas.length ?
                   misBebidas.map(el => {
                     return (
-                      <div className='card bg-dark border border-dark d-flex flex-row hover-tarjeta boton my-1' key={el._id} onClick={() => goto('/drink/' + el._id)}>
+                      <div className='card bg-dark border border-dark d-flex flex-row hover-tarjeta boton my-1' key={el._id}>
                         <img src={el.img} alt={el.nombre} width='200px' height='180px' />
                         <div className='d-flex flex-column'>
-                          <span className='text-light mx-2 my-1 d-flex align-items-center' style={{ fontSize: '22px' }}>{el.nombre} &nbsp;<FcEmptyTrash onClick={() => deleteDrink(el._id, el.nombre)} title='eliminar bebida' className='boton' /></span>
+                          <div className='d-flex flex-row'>
+                            <span className='text-light mx-2 my-1 d-flex align-items-center boton' onClick={() => goto('/drink/' + el._id)} style={{ fontSize: '22px' }}>{el.nombre}</span>
+                            &nbsp;<FcEmptyTrash onClick={() => deleteDrink(el._id, el.nombre)} title='eliminar bebida' className='boton' />
+                          </div>
                           <span className='text-light mx-2 my-1' style={{ fontSize: '17px' }}>Categoría: <strong>{el.categoria}</strong></span>
                           <span className='text-light mx-2 my-1' style={{ fontSize: '17px' }}>Es: <strong>{el.tieneAlcohol}</strong></span>
                           <div className='d-flex flex-row mx-2 my-1 text-light align-items-center' title='comentarios'>
